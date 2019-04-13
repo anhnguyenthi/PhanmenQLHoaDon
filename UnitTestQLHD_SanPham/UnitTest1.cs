7fa8e927a898fbff8efe1aff1ab12510014f5320
anhnguyenthi <1554050007anh@ou.edu.vn>
2019-04-13 18:39:31 +0700

    hghgujh

---
+++ UnitTestQLHD_SanPham/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestQLHD_SanPham
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- UnitTestQL_HD/UnitTest1.cs
+++ UnitTestQL_HD/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Unittest_QLHoaDon
+namespace UnitTestQL_HD
 {
     [TestClass]
     public class UnitTest1
