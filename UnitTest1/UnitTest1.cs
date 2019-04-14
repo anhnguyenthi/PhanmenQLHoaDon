be61649338ad92562a7777092906a1e32f30b024
anhnguyenthi <1554050007anh@ou.edu.vn>
2019-04-14 13:17:03 +0700

    Tạo uint test login

---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using InvoiceManager;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class test_dangnhap
+    {
+        [TestMethod]
+        public void Test_Nhap_Dung_Tai_Khoan()
+        {
+            string actual = Kiemtradangnhap.kt("admin", "123456");
+            string expected = "0";
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void Test_Nhap_Thieu_ID_and_Pass()
+        {
+            string actual = Kiemtradangnhap.kt("", "");
+            string expected = "1";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Test_Nhap_Thieu_ID()
+        {
+            string actual = Kiemtradangnhap.kt("", "123");
+            string expected = "2";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Test_Nhap_Thieu_Pass()
+        {
+            string actual = Kiemtradangnhap.kt("an", "");
+            string expected = "3";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- UnitTestQL_HD/UnitTest1.cs
+++ UnitTestQL_HD/UnitTest1.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace UnitTestQL_HD
 {
+   
     [TestClass]
-    public class UnitTest1
+    public class test_login
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        //[TestMethod]
+        //public void Test_Nhap_Thieu_ID_and_Pass()
+        //{
+        //    string actual = checkKiemTraDangNhap.ktdangnhap("", "");
+        //    string expected = "1";
+        //    Assert.AreEqual(expected, actual);
+        //}
+        //[TestMethod]
+        //public void Test_Nhap_Thieu_ID()
+        //{
+        //    string actual = checkKiemTraDangNhap.ktdangnhap("", "123");
+        //    string expected = "2";
+        //    Assert.AreEqual(expected, actual);
+        //}
+        //[TestMethod]
+        //public void Test_Nhap_Thieu_Pass()
+        //{
+        //    string actual = checkKiemTraDangNhap.ktdangnhap("an", "");
+        //    string expected = "3";
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
