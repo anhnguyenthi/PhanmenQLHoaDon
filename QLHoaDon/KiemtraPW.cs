55d22a852e49ed28ca17b4d076ba4f035adea79c
anhnguyenthi <1554050007anh@ou.edu.vn>
2019-04-14 13:58:45 +0700

    unit test đổi mk

---
+++ QLHoaDon/KiemtraPW.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManager
+{
+    public class KiemtraPW
+    {
+        
+            public static string check_changepassword(string oldpass, string newpass, string confpass , string mainPass)
+            {
+
+                if (newpass.Length - 1 < 5) { return "Xác nhận mật khẩu không đúng"; }
+                else if (newpass.Length - 1 > 30) { return "Mật khẩu mới quá dài"; }
+                else if (newpass != confpass) { return "Xác nhận mật khẩu không đúng"; }
+                else if (oldpass != mainPass) { return "Nhập sai mật khẩu cũ"; }
+            return "Đổi mật khẩu thành công";
+            }
+        
+    }
+
+}
+
